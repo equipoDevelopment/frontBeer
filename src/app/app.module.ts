4cf8a4a823cde1497021f6898415f8c871d12523
polSoMo <polSoMo>
2023-02-01 20:24:35 +0100

    oneProduct

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,11 +30,13 @@ import { ProductComponent } from './components/product/product.component';
     ShopComponent,
     FooterComponent,
     ProductComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    HttpClientModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
